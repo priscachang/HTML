701dc5c2974d3f1d7224231d602eed65450a3dbd
Prisca Chang <78246888+priscachang@users.noreply.github.com>
2026-09-01 10:10:56 -0400

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,8 @@
+def main(args):
+    # All node outputs, system variables, and environment variables
+    # are available automatically:
+    #   args["nodes"]["llm-0"]["completion"]
+    #   args["system_vars"]["message_id"]
+    #   args["env_vars"]["MY_KEY"]
+
+    return {"message": "Hello from Code Node!"}
