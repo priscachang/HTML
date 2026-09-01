6d4283826d7c4cad96cb5f6e372a1ddaea65f7f4
Prisca Chang <78246888+priscachang@users.noreply.github.com>
2026-09-01 10:01:53 -0400

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-def main(args):
-    # All node outputs, system variables, and environment variables
-    # are available automatically:
-    #   args["nodes"]["llm-0"]["completion"]
-    #   args["system_vars"]["message_id"]
-    #   args["env_vars"]["MY_KEY"]
-
-    return {"message": "Hello from Code Node!"}
